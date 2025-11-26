0eb8ed2f7b016739b6b542e12c9fc163c1de25b0
gaiaprojectsol <gaiaprojectsol@gmail.com>
2025-11-27 01:03:17 +1030

    Add files via upload

---
+++ config/env.js
@@ -0,0 +1,2 @@
+// env.js — Set SDK mode
+export const USE_MOCK_CHAIN = true; // true = test mode
---
+++ config/traitMap.js
@@ -0,0 +1,14 @@
+// traitMap.js — How NFT traits convert into game stats
+
+export const TRAIT_MAP = {
+    STR:  { scale: 1 },
+    AGI:  { scale: 1 },
+    INT:  { scale: 1 },
+    WIS:  { scale: 1 },
+    LUCK: { scale: 1 },
+    CHA:  { scale: 1 },
+    END:  { scale: 1 },
+    SPD:  { scale: 1 },
+    VIT:  { scale: 1 },
+    CRE:  { scale: 1 }
+};
