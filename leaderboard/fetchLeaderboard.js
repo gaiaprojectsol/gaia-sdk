cb462efc8d3b0d50c368c2673626588fc629175f
gaiaprojectsol <gaiaprojectsol@gmail.com>
2025-11-27 01:07:09 +1030

    Delete leaderboard directory

--- leaderboard/fetchLeaderboard.js
+++
@@ -1,6 +0,0 @@
-import { LEADERBOARD_ENDPOINTS } from "../../config/constants.js";
-
-export async function fetchLeaderboardReal(gameId) {
-    const res = await fetch(`${LEADERBOARD_ENDPOINTS.fetch}?gameId=${gameId}`);
-    return await res.json();
-}
